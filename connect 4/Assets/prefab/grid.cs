c885866c4d9ae5d4bf39fd25262d02120c78274d
Luthere15 <le3junkmail@gmail.com>
2018-10-12 00:02:27 -0400

    instantiate

---
+++ connect 4/Assets/prefab/grid.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grid : MonoBehaviour
+{
+    [Header("set in Inspector")]
+    public GameObject prefabGamePiece;
+
+    [Header("ser Dynamcally")]
+    public Vector3 launchPos;
+    public GameObject gamepiece;
+
+    void Awake()
+    {
+       // launchPos = launchPosTrans.position;
+    }
+    void OnMouseEnter()
+    {
+        print("game piece enter()");
+    }
+    void OnMouseExit()
+    {
+        print("game piece exit()");
+    }
+
+    void OnMouseDown()
+    {
+        gamepiece = Instantiate(prefabGamePiece) as GameObject;
+        gamepiece.transform.position = launchPos;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
